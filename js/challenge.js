89c86736ba9e6a156ec571a232f6bb738a46608c
jessesbyers <jesse.s.byers@gmail.com>
2020-02-21 10:09:56 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -83,35 +83,33 @@ document.addEventListener("DOMContentLoaded", () => {
 // WORKS, BUT ONLY ONCE - pause the counter
 // WORKS - the pause button should then show the text "resume."
     document.getElementById("pause").addEventListener("click", () => {
-        const pause = document.getElementById("pause").innerText;
+        let button = document.getElementById("pause").innerText;
+        let counter = document.getElementById("counter").innerText;
 
-        switch (pause) {
-            case "pause":
-                function pause() {
-                    clearInterval(interval);
-                    document.getElementById("pause").innerText = "resume"
-                };
+        function pause() {
+            clearInterval(interval);
+            document.getElementById("pause").innerText = "resume"
+        };
 
+        function resume() {
+            function autoIncrement(){
+                counter++;
+                document.getElementById("counter").innerText = counter;
+            }
+        
+            let interval = setInterval(autoIncrement, 1000);
+            document.getElementById("pause").innerHTML = "pause"
+        };
+
+        if (button === "pause") {
                 // NOT YET - disable all buttons except the pause button
                 pause();
-                return
-
-// WORKS, BUT ONLY ONCE When 'resume' is clicked, it should restart the counter and re-enable the buttons.
-            case "resume":
-                let counter = document.getElementById("counter").innerText;
-                function resume() {
-                    function autoIncrement(){
-                        counter++;
-                        document.getElementById("counter").innerText = counter;
-                    }
-                
-                    let interval = setInterval(autoIncrement, 1000);
-                    document.getElementById("pause").innerHTML = "pause"
-                };
+        } else if (button === "resume") {
+                // WORKS, BUT ONLY ONCE When 'resume' is clicked, it should restart the counter and re-enable the buttons.
                 resume();
-                return
         };
     });
+
 // WORKS: As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
     document.getElementById("submit").addEventListener("click", () => {
         event.preventDefault();
@@ -132,26 +130,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
-
-
-
-
-            // iterate through liList and check if each item starts with counter number
-            // if not,  
-            //  li.innerHTML = `${counter} has been liked ${n} times`;
-                // ul.appendChild(li);
-            // if yes, then
-                // split string and set n to 5th element
-                //  li.innerHTML = `${counter} has been liked ${n} times`;
-
-
-
-
-            // NOT YET: I should see count of the number of 'likes' associated with that number.
-                // add conditional:
-                // if counter number is already in li.innerHTML, then increment n by 1
-
-            // li.innerHTML = `${counter} has been liked ${n} times`;
-            // ul.appendChild(li);
