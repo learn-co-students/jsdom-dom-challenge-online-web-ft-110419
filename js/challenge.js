c7bfd1954f14166b9fedab51ea52ecc1152a3646
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-27 23:38:51 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -11,28 +11,39 @@ function increaseCounter() {
 }
 
 function decreaseCounter() {
-    counter.innerText = parseInt(counter.innerText) - 1;    
+    counter.innerText = parseInt(counter.innerText) - 1;
 }
 
 function like() {
     let li = document.createElement('li')
-    likeButton = document.createElement('BUTTON')
+    let likeButton = document.createElement('button')
+    likeButton.id = `like-button-${likes.children.length}`
     likeButton.innerText = "❤️ 1"
     li.innerText = counter.innerText + " "
-    likes.appendChild(li)
-    li.appendChild(likeButton)
+    likes.appendChild(li);
+    li.appendChild(likeButton);
 }
 
-function addLikes() {
-    let likes = parseInt(likeButton.innerText.split(" ")[1])
-    return likes + 1;
+function addLike(button) {
+    let likes = parseInt(button.innerText.split(" ")[1])
+    button.innerText = `❤️ ${likes + 1}`
 }
+
+likes.addEventListener('click', function(evt) {
+    if (evt.target && evt.target.matches('li > button')) {
+        addLike
+    }
+});
 
 function addEventListeners() {
    plusButton.addEventListener('click', increaseCounter);
    minusButton.addEventListener('click', decreaseCounter);
    heartButton.addEventListener('click', like);
-   likeButton.addEventListener('click', addLikes)
+   likes.addEventListener('click', function(evt) {
+    if (evt.target && evt.target.matches('li > button')) {
+        addLike(evt.target)
+    }
+    });
 }
 
 let timer = setInterval(increaseCounter, 1000);
